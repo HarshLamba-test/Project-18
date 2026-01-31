f7228447e5899d09cd560428955bc2393c91b50c
HarshLamba-test <lamba.harsh24@gmail.com>
2026-01-31 09:48:56 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+
+  region = "eu-west-1"
+
+}
+
+
+
+resource "aws_s3_bucket" "example_bucket" {
+
+  bucket_prefix = "capstone-project-18-"
+
+  
+
+  tags = {
+
+    Environment = "Production"
+
+    Project     = "GitOps"
+
+  }
+
+}
